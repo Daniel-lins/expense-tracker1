e49727345fa46c7fa48825f19d7ab3962e49236f
Daniel-lins <dlins300@gmail.com>
2022-01-24 23:22:26 -0300

    primeiro commit

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item'
+export const items: Item[] = [
+    { date: new Date(2022, 1, 24), category: 'food', title: 'mcDonalds', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2021, 9, 15), category: 'rent', title: 'Alugue Apt', value: 2300 },
+    { date: new Date(2021, 9, 16), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { date: new Date(2021, 10, 18), category: 'food', title: 'mcDonalds', value: 32.12 },
+
+]
